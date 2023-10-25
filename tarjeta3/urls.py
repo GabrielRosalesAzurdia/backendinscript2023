f5950de8e49ab97f58c849c735a5b72d86332ec6
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2023-10-24 21:09:45 -0600

    link tarjeta 3

--- tarjeta3/urls.py
+++ tarjeta3/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from .views import Tarjeta3View
 
 router = routers.DefaultRouter()
-router.register(r'tarjeta2', Tarjeta3View)
+router.register(r'tarjeta3', Tarjeta3View)
 
 urlpatterns = [
     path('', include(router.urls)) ,
